6f6535f27a7f5e719e0960519da9d1e6189d4608
huhouhuaM <huhouhuaM@outlook.com>
2023-01-27 02:21:32 +0800

    add BindJSONNumber and  BindJSONDisallowUnknownFields a method in context

--- web/v3/context.go
+++ web/v3/context.go
@@ -6,10 +6,10 @@ import (
 	"net/http"
 )
 
-var (
-	JSONUseNumber             = true
-	JSONDisallowUnknownFileds = true
-)
+//var (
+//	JSONUseNumber             = true
+//	JSONDisallowUnknownFileds = true
+//)
 
 type Context struct {
 	Request    *http.Request
@@ -17,24 +17,51 @@ type Context struct {
 	PathParams map[string]string
 }
 
-func (c *Context) BindJSON(val any) error {
-	if val == nil {
-		return errors.New("web:输入为 nil")
-	}
+func (c *Context) newDecoder() (error, *json.Decoder) {
 	if c.Request.Body == nil {
-		return errors.New("web:body为 nil")
+		return errors.New("web: body为 nil"), nil
 	}
-	decoder := json.NewDecoder(c.Request.Body)
-	//表示:数字就用Number来表示
-	//否则默认是 float64
-	if JSONUseNumber {
-		decoder.UseNumber()
+	return nil, json.NewDecoder(c.Request.Body)
+}
+
+func (c *Context) BindJSON(val any) error {
+	if val == "" {
+		return errors.New("web: 输入为 nil")
 	}
-	//如果有一个未知的字段，就会保错
-	//比如说你 User只有Name和Email两个字段
-	//JSON 里面额外多了一个Age字段，那么就会报错
-	if JSONDisallowUnknownFileds {
-		decoder.DisallowUnknownFields()
+	err, decoder := c.newDecoder()
+	if err != nil {
+		return err
 	}
+	return decoder.Decode(val)
+}
+
+// 表示:数字就用Number来表示
+// 否则默认是 float64
+func (c *Context) BindJSONNumber(val any) error {
+	if val == "" {
+		return errors.New("web: 输入为 nil")
+	}
+	err, decoder := c.newDecoder()
+	if err != nil {
+		return err
+	}
+	decoder.UseNumber()
+
+	return decoder.Decode(val)
+}
+
+// 如果有一个未知的字段，就会保错
+// 比如说你 User只有Name和Email两个字段
+// JSON 里面额外多了一个Age字段，那么就会报错
+func (c *Context) BindJSONDisallowUnknownFields(val any) error {
+	if val == "" {
+		return errors.New("web: 输入为 nil")
+	}
+	err, decoder := c.newDecoder()
+	if err != nil {
+		return err
+	}
+	decoder.DisallowUnknownFields()
+
 	return decoder.Decode(val)
 }
