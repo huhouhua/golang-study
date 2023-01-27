76bcdc1f7976bec65de0294c1f2efd90a70285ba
huhouhuaM <huhouhuaM@outlook.com>
2023-01-27 21:18:07 +0800

    add withTimeout test

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
 func main() {
+	WithTimeout()
+
+}
+
+func WithTimeout() {
+	context, cancel := context.WithTimeout(context.TODO(), time.Second*5)
+
+	defer cancel()
+	for {
+		select {
+		case <-context.Done():
+			fmt.Println("完成！")
+			return
+		default:
+			fmt.Println("default!")
+		}
+	}
+
 }
