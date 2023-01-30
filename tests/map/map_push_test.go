7c8a89724c0fbcc8c8021882b41259a9992ded54
huhouhuaM <huhouhuaM@outlook.com>
2023-01-30 22:42:29 +0800

    add map test

--- tests/map/map_push_test.go
+++ tests/map/map_push_test.go
@@ -2,6 +2,7 @@ package _map
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -10,4 +11,26 @@ func TestTravelMap(t *testing.T) {
 	for k, v := range m1 {
 		fmt.Printf("k:%s v:%d \n", k, v)
 	}
+}
+
+func TestObjEqual(t *testing.T) {
+
+	u1 := User{
+		Name: "张三",
+		Id:   123,
+	}
+	u2 := User{
+		Name: "张三",
+		Id:   123,
+	}
+	if reflect.ValueOf(u1) == reflect.ValueOf(u2) {
+		t.Log("相等")
+	} else {
+		t.Log("不相等")
+	}
+}
+
+type User struct {
+	Id   int
+	Name string
 }
