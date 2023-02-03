6933673a3e1a558f8421c9a3f2470e256ec06ec8
huhouhuaM <huhouhuaM@outlook.com>
2023-02-03 20:52:14 +0800

    add lock tests

--- tests/thread/lock/lock_test.go
+++ tests/thread/lock/lock_test.go
@@ -1,6 +1,8 @@
 package lock
 
 import (
+	"context"
+	"fmt"
 	"sync"
 	"testing"
 	"time"
@@ -18,4 +20,52 @@ func TestCounter(t *testing.T) {
 	}
 	time.Sleep(time.Second * 1)
 	t.Logf("counter = %d", counter)
+}
+
+func TestLockTest(t *testing.T) {
+	ctx, _ := context.WithTimeout(context.TODO(), time.Second*10)
+	c := &contextLock{
+		users: []*user{&user{}},
+	}
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			c.updateName(i)
+		}(i)
+		for i := 0; i < 10; i++ {
+			go func(i int) {
+				c.updateName(i)
+			}(i)
+		}
+	}
+	select {
+	case <-ctx.Done():
+		for _, u := range c.users {
+			t.Logf("%v \n", u)
+		}
+	}
+}
+
+type contextLock struct {
+	sync.Mutex
+	users []*user
+}
+
+func (c *contextLock) updateName(id int) {
+	defer c.Unlock()
+	c.Lock()
+
+	for _, u := range c.users {
+		if u.Id == id {
+			return
+		}
+	}
+	c.users = append(c.users, &user{
+		Name: fmt.Sprintf("张三 %b", id),
+		Id:   id,
+	})
+}
+
+type user struct {
+	Id   int
+	Name string
 }
