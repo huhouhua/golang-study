d9b477e055069b8cd379f017e51e5aa927f07f82
huhouhuaM <huhouhuaM@outlook.com>
2023-02-06 20:14:23 +0800

    add WithCancel test

--- main.go
+++ main.go
@@ -7,22 +7,45 @@ import (
 )
 
 func main() {
-	WithTimeout()
-
+	//WithTimeout()
+	WithCancel()
 }
 
 func WithTimeout() {
 
-	context, cancel := context.WithTimeout(context.TODO(), time.Second*5)
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*5)
 
 	defer cancel()
 	for {
 		select {
-		case <-context.Done():
-			fmt.Println("完成！")
+		case <-ctx.Done():
+			fmt.Println("5秒到了 完成！")
 			return
 		default:
 			fmt.Println("default!")
+		}
+	}
+
+}
+func WithCancel() {
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		for i := 4; i > 0; i-- {
+			time.Sleep(time.Second)
+			fmt.Printf("还剩%d秒 \n", i)
+		}
+		defer cancel()
+	}()
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("被取消！")
+			return
+		default:
+			fmt.Println("等待中!")
+			time.Sleep(time.Second)
 		}
 	}
 
